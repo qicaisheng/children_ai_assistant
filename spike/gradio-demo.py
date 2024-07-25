bd68446d5535d47c404892329ece96492b88c905
qicaisheng <qicaisheng@qq.com>
2024-07-25 14:17:07 +0800

    modify gradio to create chatbot which call doubao api

--- spike/gradio-demo.py
+++ spike/gradio-demo.py
@@ -1,12 +1,29 @@
+from openai import OpenAI
 import gradio as gr
+import os
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
+client = OpenAI(
+    api_key = os.environ.get("ARK_API_KEY"),
+    base_url = "https://ark.cn-beijing.volces.com/api/v3",
 )
 
-demo.launch()
+def predict(message, history):
+    history_openai_format = []
+    for human, assistant in history:
+        history_openai_format.append({"role": "user", "content": human })
+        history_openai_format.append({"role": "assistant", "content":assistant})
+    history_openai_format.append({"role": "user", "content": message})
+  
+    response = client.chat.completions.create(model=os.environ.get("MODEL_ENDPOINT_ID"),
+    messages= history_openai_format,
+    temperature=1.0,
+    stream=True)
+
+    partial_message = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+              partial_message = partial_message + chunk.choices[0].delta.content
+              yield partial_message
+
+gr.ChatInterface(predict).launch()
