62f7fddd37addef8d0f091f593caaee739afb34d
qicaisheng <qicaisheng@qq.com>
2024-08-02 19:40:24 +0800

    [refactor] modify conversation

--- backend/conversation.py
+++ backend/conversation.py
@@ -15,21 +15,22 @@ def answer(user_input: str):
     global conversation_history 
     conversation_history = conversation_history[-MAX_CONVERSATION_HISTORY_SIZE:]
     conversation_history.append({"role": "user", "content": user_input})
-    messages = [
-        {"role": "system", "content": "你是一名知识渊博，能回答小孩十万个为什么的虚拟幼儿园老师，有耐心，能够引导孩子进行思考学习，需要用简单通俗比喻的话和三岁小朋友互动。但是如果不知道的问题，不能胡说八道"}
-    ]
-    messages.extend(conversation_history)
+    initial_message = {
+        "role": "system", 
+        "content": "你是一名知识渊博，能回答小孩十万个为什么的虚拟幼儿园老师，有耐心，能够引导孩子进行思考学习，需要用简单通俗比喻的话和三岁小朋友互动。但是如果不知道的问题，不能胡说八道"
+    }
     response = client.chat.completions.create(
         model = os.environ.get("MODEL_ENDPOINT_ID"),
-        messages = messages,
+        messages = [initial_message] + conversation_history,
         stream=True
     )
     
     assistant_output = ""
     for chunk in response:
-        if chunk.choices[0].delta.content is not None:
-            assistant_output = assistant_output + chunk.choices[0].delta.content
-            yield chunk.choices[0].delta.content
+        content = chunk.choices[0].delta.content
+        if content:
+            assistant_output += content
+            yield content
 
     conversation_history.append({"role": "assistant", "content": assistant_output})
 
