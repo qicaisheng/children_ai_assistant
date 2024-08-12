810874d15d7b583879d405d7d420283e23ecbd86
qicaisheng <qicaisheng@qq.com>
2024-08-12 22:41:44 +0800

    feat: add refresh and summary button

--- spike/gradio-demo-with_audio.py
+++ spike/gradio-demo-with_audio.py
@@ -9,6 +9,9 @@ client = OpenAI(
     base_url="https://ark.cn-beijing.volces.com/api/v3",
 )
 
+chat_history = []
+
+
 def click_js():
     return """function audioRecord() {
     var xPathRes = document.evaluate ('//*[contains(@class, "record")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null); 
@@ -72,6 +75,11 @@ with gr.Blocks() as demo1:
             if chunk.choices[0].delta.content is not None:
                 history[-1][1] += chunk.choices[0].delta.content
                 yield history
+        global chat_history
+        chat_history = history
+        print('-------')
+        print(chat_history)
+        print('-------')
 
     input_msg.submit(user, inputs=[input_msg, chatbot], outputs=[input_msg, chatbot], queue=False).then(
         bot, inputs=chatbot, outputs=chatbot
@@ -98,80 +106,20 @@ with gr.Blocks() as demo1:
     )
 
 with gr.Blocks() as demo2:
-    chatbot = gr.Chatbot()
-    input_audio_button = gr.Button("Speak")
-    input_msg = gr.Textbox()
-    input_audio = gr.Audio(
-        label="输入音频",
-        sources=["microphone"],
-        type='filepath',
-        waveform_options=gr.WaveformOptions(
-            waveform_color="#01C6FF",
-            waveform_progress_color="#0066B4",
-            skip_length=2,
-            show_controls=False,
-        ),
-        editable=False,
-        interactive=True,
-        elem_id='input_audio_elem',
-        visible=False
-    )
-
-    input_audio_button.click(fn=action, inputs=input_audio_button, outputs=input_audio_button).\
-        then(fn=lambda: None, js=click_js())
-
-    output_audio = gr.Audio(autoplay=True, label="输出音频")
-
-    def user(user_message, history):
-        return "", history + [[user_message, None]]
-
-    def bot(history):
-        MAX_CONVERSATION_ROUND = 3
-        initial_message = {
-            "role": "system", 
-            "content": "你是一名知识渊博，能回答小孩十万个为什么的虚拟幼儿园老师，有耐心，能够引导孩子进行思考学习，需要用简单通俗比喻的话和三岁小朋友互动。但是如果不知道的问题，不能胡说八道"
-        }
-        history_openai_format = []
-        for human, assistant in history[-MAX_CONVERSATION_ROUND:]:
-            history_openai_format.append({"role": "user", "content": human})
-            if assistant != None:
-                history_openai_format.append({"role": "assistant", "content": assistant})
-        response = client.chat.completions.create(
-            model = os.environ.get("MODEL_ENDPOINT_ID"),
-            messages = [initial_message] + history_openai_format,
-            temperature = 1.0,
-            stream = True,
-        )
-
-        history[-1][1] = ""
-        for chunk in response:
-            if chunk.choices[0].delta.content is not None:
-                history[-1][1] += chunk.choices[0].delta.content
-                yield history
-
-    input_msg.submit(user, inputs=[input_msg, chatbot], outputs=[input_msg, chatbot], queue=False).then(
-        bot, inputs=chatbot, outputs=chatbot
-    )
-
-    def clear_audio(audio):
-        return None
+    refresh_btm = gr.Button("Refresh")
     
-    async def speak(history):
-        print('-------speak start1-------')
-        print(history)
-        print('-------speak start2-------')
-        text = history[-1][1]
-        return await tts.speak(text)
+    chatbot2 = gr.Chatbot(value=chat_history)
 
-    input_audio.stop_recording(fn=asr.recognize, inputs=input_audio, outputs=input_msg).then(
-        user, inputs=[input_msg, chatbot], outputs=[input_msg, chatbot], queue=False
-    ).then(
-        bot, inputs=chatbot, outputs=chatbot
-    ).then(
-        speak, inputs=chatbot, outputs=output_audio
-    ).then(
-        clear_audio, inputs=input_audio, outputs=input_audio
-    )
+    summary_btm = gr.Button("Summary")
+
+    refresh_btm.click(lambda: chat_history, outputs=chatbot2)
+
+    def summary():
+        print("summary")
+
+    refresh_btm.click(summary, outputs=chatbot2)
+
+
 
 demo = gr.TabbedInterface([demo1, demo2], ["语音对话", "对话历史"])
 demo.launch()
