cc40aa5c7848e4a07580c82b371f0e8670ef8541
qicaisheng <qicaisheng@qq.com>
2024-08-14 16:44:09 +0800

    feat: modfiy button name

--- spike/gradio_parents_tab.py
+++ spike/gradio_parents_tab.py
@@ -4,15 +4,15 @@ import database
 
 
 with gr.Blocks() as page:
-    refresh_btm = gr.Button("刷新")
+    refresh_btn = gr.Button("刷新对话历史")
     chatbots = {}
 
-    @gr.render(triggers=[refresh_btm.click])
+    @gr.render(triggers=[refresh_btn.click])
     def render_chatbots():
         for _role, _history in database.chat_history.items():
             chatbots[_role] = gr.Chatbot(value=_history, label=f"小朋友和{_role}的对话记录")
 
-    summary_btn = gr.Button("查看对话总结")
+    summary_btn = gr.Button("刷新对话总结")
     summary_msg = gr.Markdown(label="小朋友对话总结")
 
 
