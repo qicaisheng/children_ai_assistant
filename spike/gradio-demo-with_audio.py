8bfe03931cf25d25684e2f92bf9fcd311d2d9dea
qicaisheng <qicaisheng@qq.com>
2024-08-14 17:43:43 +0800

    feat: modfiy home page layout

--- spike/gradio-demo-with_audio.py
+++ spike/gradio-demo-with_audio.py
@@ -49,26 +49,30 @@ with gr.Blocks() as childrend_page:
     )
 
     introduction_msg = gr.Textbox(label="介绍", value=database.get_introduction(role=role))
+    
+    with gr.Row():
+        with gr.Column():
+            chatbot = gr.Chatbot(label="对话框", value=database.get_history(role=role))
+        with gr.Column():
+            input_msg = gr.Textbox(label="文本输入框")
+            input_audio = gr.Audio(
+                label="输入音频",
+                sources=["microphone"],
+                type='filepath',
+                waveform_options=gr.WaveformOptions(
+                    waveform_color="#01C6FF",
+                    waveform_progress_color="#0066B4",
+                    skip_length=2,
+                    show_controls=False,
+                ),
+                editable=False,
+                interactive=True,
+                elem_id='input_audio_elem'
+            )
+            output_audio = gr.Audio(autoplay=True, label="输出音频")
 
-    chatbot = gr.Chatbot(value=database.get_history(role=role))
+
     input_audio_button = gr.Button("按住说话")
-    input_msg = gr.Textbox()
-    input_audio = gr.Audio(
-        label="输入音频",
-        sources=["microphone"],
-        type='filepath',
-        waveform_options=gr.WaveformOptions(
-            waveform_color="#01C6FF",
-            waveform_progress_color="#0066B4",
-            skip_length=2,
-            show_controls=False,
-        ),
-        editable=False,
-        interactive=True,
-        elem_id='input_audio_elem'
-    )
-    output_audio = gr.Audio(autoplay=True, label="输出音频")
-
 
     def refresh_roles_ropdown():
         return gr.Dropdown(choices=database.get_saved_roles(), interactive=True)
