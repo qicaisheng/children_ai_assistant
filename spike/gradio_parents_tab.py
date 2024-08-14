91a627d1dc9c782e16c89ffda4903943f4c393c4
qicaisheng <qicaisheng@qq.com>
2024-08-14 16:43:24 +0800

    feat: dynamic render chatbots in parents page

--- spike/gradio_parents_tab.py
+++ spike/gradio_parents_tab.py
@@ -7,18 +7,14 @@ with gr.Blocks() as page:
     refresh_btm = gr.Button("刷新")
     chatbots = {}
 
-    for _role, _history in database.chat_history.items():
-        chatbots[_role] = gr.Chatbot(value=_history, label=f"小朋友和{_role}的对话记录")
-
-
-    def refresh_chatbots():
-        print(database.chat_history)
+    @gr.render(triggers=[refresh_btm.click])
+    def render_chatbots():
+        for _role, _history in database.chat_history.items():
+            chatbots[_role] = gr.Chatbot(value=_history, label=f"小朋友和{_role}的对话记录")
 
     summary_btn = gr.Button("查看对话总结")
     summary_msg = gr.Markdown(label="小朋友对话总结")
 
-
-    refresh_btm.click(lambda : refresh_chatbots())
 
     def summary():
         if len(database.summary) == 0:
