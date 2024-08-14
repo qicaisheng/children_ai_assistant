97c3533392d776c3fea6b59ace6974a40b00c759
qicaisheng <qicaisheng@qq.com>
2024-08-14 22:35:55 +0800

    feat: add deepseek

--- spike/llm_client.py
+++ spike/llm_client.py
@@ -12,16 +12,23 @@ zhipu_client = OpenAI(
     base_url="https://open.bigmodel.cn/api/paas/v4/",
 )
 
+deepseek_client = OpenAI(
+    api_key=os.environ.get("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com",
+)
+
 class LLM_MODEL(Enum):
     DOUBAO = "DOUBAO"
     ZHIPU = "ZHIPU"
+    DEEPSEEK = "DEEPSEEK"
 
 _mapping = {
     str(LLM_MODEL.DOUBAO): {"client": doubao_client, "model": os.environ.get("MODEL_ENDPOINT_ID")},
     str(LLM_MODEL.ZHIPU): {"client": zhipu_client, "model": "GLM-4-AirX"},
+    str(LLM_MODEL.DEEPSEEK): {"client": deepseek_client, "model": "deepseek-chat"},
 }
 
-selected_llm = LLM_MODEL.DOUBAO
+selected_llm = LLM_MODEL.DEEPSEEK
 
 def get_client():
     _client = _mapping.get(str(selected_llm)).get('client')
