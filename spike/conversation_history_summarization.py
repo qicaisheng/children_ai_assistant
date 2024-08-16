781fb20c831d04acd5508ddcbf07bc989db101e0
qicaisheng <qicaisheng@qq.com>
2024-08-16 16:16:57 +0800

    feat: modify prompt

--- spike/conversation_history_summarization.py
+++ spike/conversation_history_summarization.py
@@ -1,19 +1,20 @@
 import llm_client
 import os
+import database
 
 
-_DEFAULT_SUMMARIZER_TEMPLATE = """基于给定当前总结和新的对话生成新的总结，其中User代表小朋友，Assistant代表{role}
+_DEFAULT_SUMMARIZER_TEMPLATE = """基于给定当前总结和新的对话生成新的{user}和{role}的对话总结，其中User代表{user}，Assistant代表{role}。
 
 例子
 当前总结:
-User询问Assistant对人工智能的看法。Assistant认为人工智能是一种积极的力量。
+{user}询问{role}对人工智能的看法。{role}认为人工智能是一种积极的力量。
 
 新的对话：
 User: 你为什么认为人工智能是一种积极的力量？
 Assistant: 因为人工智能将帮助人类发挥他们的全部潜力。
 
 新的总结：
-小朋友询问{role}对人工智能的看法。{role}认为人工智能是一种积极的力量，因为它将帮助人类发挥他们的全部潜力。
+{user}询问{role}对人工智能的看法。{role}认为人工智能是一种积极的力量，因为它将帮助人类发挥他们的全部潜力。
 例子结束。
 
 当前总结:
@@ -30,7 +31,7 @@ def generate_new_summary(current_summary: str, new_conversation_history: list, c
     for human, assistant in new_conversation_history:
         new_liens += "User: " + human + "\n"
         new_liens += "Assistant: " + assistant + "\n"
-    content = _DEFAULT_SUMMARIZER_TEMPLATE.format(summary=current_summary, new_lines=new_liens, role=current_role)
+    content = _DEFAULT_SUMMARIZER_TEMPLATE.format(summary=current_summary, new_lines=new_liens, role=current_role, user=database.user.nickname)
     completion = llm_client.get_client().chat.completions.create(
         model=llm_client.get_model(),
         messages=[
