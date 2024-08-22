4f7208374da4b2818c05d3252ff48714963faf1b
qicaisheng <qicaisheng@qq.com>
2024-08-22 14:03:52 +0800

    feat: add subscription topics

--- backend/mqtt/subscription.py
+++ backend/mqtt/subscription.py
@@ -5,7 +5,22 @@ def processTopic1(client, userdata, msg: mqtt.MQTTMessage):
     print(msg)
     print(f"Received on {msg.topic}: {msg.payload.decode()}")
 
+def processEventPost(client, userdata, msg: mqtt.MQTTMessage):
+    print(msg)
+    print(f"Received on {msg.topic}: {msg.payload.decode()}")
+
+def processCommandAck(client, userdata, msg: mqtt.MQTTMessage):
+    print(msg)
+    print(f"Received on {msg.topic}: {msg.payload.decode()}")
+
+def processDataPost(client, userdata, msg: mqtt.MQTTMessage):
+    print(msg)
+    print(f"Received on {msg.topic}: {msg.payload.decode()}")
+
 
 subscriptions={
     "test/topic1": processTopic1,
+    "/user/folotoy/+/thing/event/post": processEventPost,
+    "/user/folotoy/+/thing/command/callAck": processCommandAck,
+    "/user/folotoy/+/thing/data/post": processDataPost,
 }
