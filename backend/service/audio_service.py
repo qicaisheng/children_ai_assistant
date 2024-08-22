779027250e95d6f06f530a00553e4eed86062dfe
qicaisheng <qicaisheng@qq.com>
2024-08-22 21:41:40 +0800

    feat: integrate llm to audio_service

--- backend/service/audio_service.py
+++ backend/service/audio_service.py
@@ -1,12 +1,17 @@
 from service.streaming_asr_demo import recognize
 from service.tts_websocket_demo import speak
 import mqtt.publisher as mqtt_publisher
+from conversation import no_stream_answer
 
 async def response_to_uploaded_audio(audio_path: str):
     print(f"response_to_uploaded_audio: {audio_path}")
     input_text = recognize(audio_path)
     print(f"ASR succeed, input_text: {input_text}")
-    output_audio_path = await speak(text=input_text)
+
+    output_text = no_stream_answer(input_text)
+    print(f"LLM succeed, output_text: {output_text}")
+    
+    output_audio_path = await speak(text=output_text)
     print(f"TTS succeed, output_audio_path: {output_audio_path}")
 
     recordingId = 1
