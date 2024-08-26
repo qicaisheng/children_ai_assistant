489690c25df1fc6e0eabe389a96a5e720221a779
qicaisheng <qicaisheng@qq.com>
2024-08-26 09:46:43 +0800

    feat: should publish retry voice mq when no input text detect from speech

--- backend/service/audio_service.py
+++ backend/service/audio_service.py
@@ -4,10 +4,18 @@ from service.tts_websocket_demo import speak
 import mqtt.publisher as mqtt_publisher
 from conversation import no_stream_answer
 import config
+from core.role import get_current_role
 
 async def response_to_uploaded_audio(audio_path: str, recording_id: int):
     print(f"response_to_uploaded_audio: {audio_path}")
     input_text = await recognize(audio_path)
+
+    if input_text == "":
+        role = get_current_role()
+        mqtt_publisher.audio_play_cmd(mqtt_publisher.AudioPlayCMD(recordingId=recording_id, total=1))
+        mqtt_publisher.audio_play(mqtt_publisher.AudioPlay(recordingId=recording_id, order=1, url=role.retry_voice))
+        return
+
     print(f"ASR succeed, input_text: {input_text}")
 
     output_text = no_stream_answer(input_text)
