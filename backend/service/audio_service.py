2d19221581149cfc26c7dd972372c0b6ebb1b4ec
qicaisheng <qicaisheng@qq.com>
2024-08-26 09:47:56 +0800

    feat: use current role voice type to generate speech

--- backend/service/audio_service.py
+++ backend/service/audio_service.py
@@ -7,11 +7,12 @@ import config
 from core.role import get_current_role
 
 async def response_to_uploaded_audio(audio_path: str, recording_id: int):
+    role = get_current_role()
+
     print(f"response_to_uploaded_audio: {audio_path}")
     input_text = await recognize(audio_path)
 
     if input_text == "":
-        role = get_current_role()
         mqtt_publisher.audio_play_cmd(mqtt_publisher.AudioPlayCMD(recordingId=recording_id, total=1))
         mqtt_publisher.audio_play(mqtt_publisher.AudioPlay(recordingId=recording_id, order=1, url=role.retry_voice))
         return
@@ -21,7 +22,7 @@ async def response_to_uploaded_audio(audio_path: str, recording_id: int):
     output_text = no_stream_answer(input_text)
     print(f"LLM succeed, output_text: {output_text}")
 
-    output_audio_path = await speak(text=output_text)
+    output_audio_path = await speak(text=output_text, voice_type=role.voice_type)
     print(f"TTS succeed, output_audio_path: {output_audio_path}")
 
     mqtt_publisher.audio_play_cmd(mqtt_publisher.AudioPlayCMD(recordingId=recording_id, total=1))
