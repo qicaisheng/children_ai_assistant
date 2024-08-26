432d3619f2513f72bf29a28d272e2ca45afa1b4e
qicaisheng <qicaisheng@qq.com>
2024-08-26 09:42:36 +0800

    feat: set current role when receive client MQ message

--- backend/mqtt/subscription.py
+++ backend/mqtt/subscription.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import mqtt.event as mqtt_event
 import mqtt.publisher as mqtt_publisher
 from utils.uuid_util import get_uuid4_no_hyphen
-from core.role import get_role_by_code
+from core.role import get_role_by_code, set_current_role_code
 
 
 def processTopic1(client, userdata, msg: mqtt.MQTTMessage):
@@ -17,12 +17,15 @@ def processEventPost(client, userdata, msg: mqtt.MQTTMessage):
         print(f"Validation error: {e}")
         return
     if mqtt_event.ReceivedIdentifier.LOGIN.value == event.identifier:
+        role_code = event.outParams.get('role')
+        set_current_role_code(role_code)
         data = mqtt_publisher.UpdateTokenData(token=get_uuid4_no_hyphen())
         mqtt_publisher.update_token(data=data)
     elif mqtt_event.ReceivedIdentifier.PRESS_SMALL_BTN.value == event.identifier:
         role_code = event.outParams.get('keyCode')
         role_changed = event.outParams.get('changed') == 1
         if role_changed:
+            set_current_role_code(role_code)
             role = get_role_by_code(role_code)
             data = mqtt_publisher.UpdateStartVoiceData(url= role.self_introduction_voice, keyCode=role_code, etag="")
             mqtt_publisher.update_start_voice(data=data)
