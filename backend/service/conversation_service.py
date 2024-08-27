a3557f5191f745bb45f7bcbb4a0a0d964423ded9
qicaisheng <qicaisheng@qq.com>
2024-08-27 17:08:31 +0800

    feat: add stream_answer

--- backend/service/conversation_service.py
+++ backend/service/conversation_service.py
@@ -1,3 +1,4 @@
+from typing import Generator
 import core.llm_client as llm_client
 from core.system_prompt import get_system_prompt_by_role_code
 from core.conversation_message import get_conversation_history
@@ -15,6 +16,20 @@ def no_stream_answer(user_input: str, role_code: int) -> list[str]:
     content = completion.choices[0].message.content
     return split_text(content)
 
+def stream_answer(user_input: str, role_code: int) -> Generator[str, None, None]:
+    response = llm_client.get_client().chat.completions.create(
+        model = llm_client.get_model(),
+        messages = build_llm_request_message(user_input, role_code),
+        stream=True
+    )
+
+    output = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content:
+            output += chunk.choices[0].delta.content
+            yield output
+
+
 def build_llm_request_message(user_input: str, role_code: int) -> list[dict]:
     initial_message = {
         "role": "system", 
@@ -29,3 +44,7 @@ def build_llm_request_message(user_input: str, role_code: int) -> list[dict]:
             history_openai_format.append({"role": "assistant", "content": assistant})
     messages = [initial_message] + history_openai_format
     return messages
+
+# stream_reponse = stream_answer("你好", 1)
+# for text in stream_reponse:
+#     print(text)
