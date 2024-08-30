43c2c5f775a2c8399476a8f33e2458a16da13138
qicaisheng <qicaisheng@qq.com>
2024-08-30 18:03:35 +0800

    fix: fix typo

--- backend/core/role_start_voice.py
+++ backend/core/role_start_voice.py
@@ -23,10 +23,9 @@ def generate_retry_voices():
         audio_path = loop.run_until_complete(speak(text, role.voice_type))
         url=get_audio_url(audio_path)
         result[role.code] = url
-[]
     print(result)
 
-generate_start_voices()
+# generate_start_voices()
 """
 {1: 'http://10.10.30.164:8082/voice-dc35fa1d919c4fbab1bba4ebcd391193.mp3', 2: 'http://10.10.30.164:8082/voice-0d5d1ed530c54ebd93bc0081fb4b4690.mp3', 3: 'http://10.10.30.164:8082/voice-144675ba17a14dcdb99acca61aec655a.mp3', 4: 'http://10.10.30.164:8082/voice-d5a84ad91deb41b7bebae0da70b0fb4e.mp3', 5: 'http://10.10.30.164:8082/voice-db753a1dff2a4c28b83cdfd7d8ae8fad.mp3'}
 {1: 'http://10.10.30.164:8082/voice-765e83dc68d34cdbbb943e6682c5bab5.mp3', 2: 'http://10.10.30.164:8082/voice-cde2da13313c4e829a3f460a8cacb268.mp3', 3: 'http://10.10.30.164:8082/voice-b7c168237c75483389f7ee738b50b3b6.mp3', 4: 'http://10.10.30.164:8082/voice-783e492d3c7f4b7daf70585c130c7914.mp3', 5: 'http://10.10.30.164:8082/voice-16a57db36fe441babeb0174ee4b4edd1.mp3', 6: 'http://10.10.30.164:8082/voice-3ea36b642a9b41ecacd2a8aafe1a52d4.mp3', 7: 'http://10.10.30.164:8082/voice-7f0bffeab070435198610bff36084ed5.mp3'}
