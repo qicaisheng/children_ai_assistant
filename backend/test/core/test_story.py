10ba600607f95e7671725b5fa155eaf5e0606dc9
qicaisheng <qicaisheng@qq.com>
2024-09-03 12:34:20 +0800

    feat: add get_story_by_name

---
+++ backend/test/core/test_story.py
@@ -0,0 +1,16 @@
+from core.story import Story, get_story_by_name
+
+def test_get_story_by_name(monkeypatch):
+    monkeypatch.setattr("config.audio_base_url", "http://localhost:8082/")
+
+    story = get_story_by_name("走失的小野雁")
+
+    expected_story = {
+        "name": "走失的小野雁",
+        "audio_ids": ["走失的小野雁.mp3"],
+        "season": 1,
+        "episode": "5A"
+    }
+
+    assert story == Story(**expected_story)
+    assert story.get_audio_urls() == ["http://localhost:8082/走失的小野雁.mp3"]
