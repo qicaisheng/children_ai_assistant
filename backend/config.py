db7bfcec8a9f1c87f2762aa0fe69bb619fd6015a
qicaisheng <qicaisheng@qq.com>
2024-09-07 20:40:48 +0800

    feat: add language

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
 from service.tts.tts import TTS
 from service.asr.asr import ASR
 from core.llm_model import LLM_MODEL
+from core.language import Language
 
 # host = "10.10.30.164"
 host = "localhost"
@@ -14,3 +15,5 @@ mqtt_port = 1883
 tts = TTS.OPENAI
 asr = ASR.OPENAI
 llm = LLM_MODEL.OPENAI
+
+language = Language.EN
---
+++ backend/core/language.py
@@ -0,0 +1,9 @@
+
+
+from enum import Enum
+
+
+class Language(Enum):
+    CN = "中文"
+    EN = "英语"
+    ES = "西语"
