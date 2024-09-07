ef6b5da24ebd3853c8dfbb63d7516f15f4288c7f
qicaisheng <qicaisheng@qq.com>
2024-09-07 12:13:21 +0800

    chore: modify config

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,8 @@
 from service.tts.tts import TTS
 from service.asr.asr import ASR
 
-host = "10.10.30.164"
+# host = "10.10.30.164"
+host = "localhost"
 audio_base_url = f"http://{host}:8082/"
 audio_file_direction = "../audio"
 udp_host = host
