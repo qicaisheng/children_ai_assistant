303576bcf3ef7537afa9acbc4e76ca21ece18fc4
qicaisheng <qicaisheng@qq.com>
2024-09-09 16:46:52 +0800

    spike: add rag_llamaindex

---
+++ spike/rag_llamaindex.py
@@ -0,0 +1,7 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+response = query_engine.query("小野雁为什么去南方过冬")
+print(response)
