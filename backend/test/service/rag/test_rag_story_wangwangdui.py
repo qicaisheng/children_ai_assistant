76b5e74f6b8931b9f7ca3a81eb96bd364d3f24a1
qicaisheng <qicaisheng@qq.com>
2024-09-11 11:55:34 +0800

    refactor: remove unused code

--- backend/test/service/rag/test_rag_story_wangwangdui.py
+++ backend/test/service/rag/test_rag_story_wangwangdui.py
@@ -1,11 +1,7 @@
 import service.rag.rag_story_wangwangdui as rag_story_wangwangdui
 
 
-def test_dd():
-    assert 1==1
-
 def test_answer():
-    # rag_story_wangwangdui.build_index()
     result = rag_story_wangwangdui.answer("走失的小野雁", "为什么要走")
     assert result is not None
 
