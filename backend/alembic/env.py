113aef106503d7d1eebfd7ae9fa80db31eb06d9b
qicaisheng <qicaisheng@qq.com>
2024-09-13 11:20:17 +0800

    chore: modify alembic metadata

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 from config.config import settings
+from sqlalchemy.ext.declarative import declarative_base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +19,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+Base = declarative_base()
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
